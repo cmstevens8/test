bb1a02a539d295416a6924ab9382a5d3e411545e
Carly <carlymariestevens8@gmail.com>
2025-02-25 15:46:28 -0700

    Triplets

---
+++ triplets.py
@@ -0,0 +1,8 @@
+name1 = input("enter the first name: ")
+name1_formatted = name1.strip().capitalize()
+name2 = input("enter the second name: ")
+name2_formatted = name2.strip().capitalize()
+name3 = input("enter the third name: ")
+name3_formatted = name3.strip().capitalize()
+
+print(f"The names are: {name1_formatted}, {name2_formatted}, and {name3_formatted}")
