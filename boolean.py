833974154749d3a9aa9150eb2927a11cce5acfaf
Carly <carlymariestevens8@gmail.com>
2025-02-25 12:43:19 -0700

    first Commit

---
+++ boolean.py
@@ -0,0 +1,15 @@
+# What will u evaluate to?
+
+s = 7 + 8 - 5
+t = 3**2
+u = s <= t
+
+print(f"excerise 4, answer: {u}")
+
+# What will x evaluate to?
+
+v = 15 / 3
+w = 3**2 - 3
+x = v != w
+
+print(f"excersie 5, answer: {x}")
---
+++ float.py
@@ -0,0 +1,8 @@
+
+
+x = 12 // 4
+
+y = 5 + 6.1
+
+print(f"Type of x is {type(x)}")
+print(f"Type of y is {type(y)}")
